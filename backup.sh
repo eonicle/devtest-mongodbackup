89244d0f6c47c3f5311367154e9249264b2b7787
eonicle <eonicle@gmail.com>
2017-03-10 01:29:53 +0200

    Added backup roation
    Now will keep five latest backups.

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Job started: $(date)"
+echo "Backup started: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/backup/backup-$DATE.tar.gz"
@@ -11,4 +11,4 @@ mongodump --quiet -h $MONGO_PORT_27017_TCP_ADDR -p $MONGO_PORT_27017_TCP_PORT
 tar -zcvf $FILE dump/
 rm -rf dump/
 
-echo "Job finished: $(date)"
+echo "Backup finished: $(date)"
--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+#CRON_SCHEDULE=* */1 * * *
 LOGFIFO='/var/log/cron.fifo'
 if [[ ! -e "$LOGFIFO" ]]; then
   mkfifo "$LOGFIFO"
@@ -9,6 +10,7 @@ fi
 CRON_ENV="MONGO_PORT_27017_TCP_ADDR='$MONGO_PORT_27017_TCP_ADDR'"
 CRON_ENV="$CRON_ENV\nMONGO_PORT_27017_TCP_PORT='$MONGO_PORT_27017_TCP_PORT'"
 echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+echo -e "ls /backup -t | tail -n +6 | xargs -I {} rm -- {} > $LOGFIFO 2>&1" | crontab -
 crontab -l
 cron
 tail -f "$LOGFIFO"
