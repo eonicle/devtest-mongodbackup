8c74617e4565b7a68b7872d6c1bea4d52db19b55
eonicle <eonicle@gmail.com>
2017-03-10 01:47:58 +0200

    Rotation fix

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ fi
 CRON_ENV="MONGO_PORT_27017_TCP_ADDR='$MONGO_PORT_27017_TCP_ADDR'"
 CRON_ENV="$CRON_ENV\nMONGO_PORT_27017_TCP_PORT='$MONGO_PORT_27017_TCP_PORT'"
 echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
-echo -e "ls /backup -t | tail -n +6 | xargs -I {} rm -- {} > $LOGFIFO 2>&1" | crontab -
+echo -e "$CRON_ENV\n$CRON_SCHEDULE ls /backup -t | tail -n +6 | xargs -I {} rm -- {} > $LOGFIFO 2>&1" | crontab -
 crontab -l
 cron
 tail -f "$LOGFIFO"
