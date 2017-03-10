309dfd400d656ee67a952d7e1912dd12047f7a81
eonicle <eonicle@gmail.com>
2017-03-10 02:21:13 +0200

    fxing rotation

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ if [[ ! -e "$LOGFIFO" ]]; then
 fi
 CRON_ENV="MONGO_PORT_27017_TCP_ADDR='$MONGO_PORT_27017_TCP_ADDR'"
 CRON_ENV="$CRON_ENV\nMONGO_PORT_27017_TCP_PORT='$MONGO_PORT_27017_TCP_PORT'"
-echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1 $CRON_ENV\n$CRON_SCHEDULE ls /backup -t | tail -n +6 | xargs -I {} rm -- {} > $LOGFIFO 2>&1" | crontab -
+echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1\n$CRON_SCHEDULE ls /backup -t | tail -n +6 | xargs -I {} rm -- {} > $LOGFIFO 2>&1 " | crontab -
 crontab -l
 cron
 tail -f "$LOGFIFO"
